0b8a2c019607733836dea06dcbe394e26d7b1555
k-hyeongyu <kampher@naver.com>
2025-05-23 14:53:11 +0900

    삭제 기능 추가

--- src/newsBlog/NewsBlog.js
+++ src/newsBlog/NewsBlog.js
@@ -51,7 +51,7 @@ function NewsBlog() {
                     let temp = [...news];
                     temp.push(inputText);
                     setNews(temp); //뉴스가 새로 추가되었을 경우에 새로운 뉴스의 좋아요도 가능하게 배열을 생성
-                    
+
                     likeCountArr.push(0);
                     //배열state변슈 바꾸는 법 : 배열 복사 -> 값 변경/ 추가 / 삭제 -> set함수로 원래 배열에 저장
                     setInputText(''); //공백 -> input value= {inpuText}
@@ -73,8 +73,18 @@ function NewsBlog() {
                                 temp[index] += 1;
                                 setLikeCountArr(temp);
 
+
                             }}>❤</span>{likeCountArr[index]}</h4>
                             <p>내용 무</p>
+                            <button onClick={() => {
+                                let tempnews = [...news];
+
+                                tempnews.splice(index, 1);
+                                likeCountArr.splice(index,1);
+
+                                setNews(tempnews);
+
+                            }}>삭제</button>
                         </div>
                     )
 
