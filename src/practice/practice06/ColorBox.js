3421b90ade2b55c09188ecfcec6c984995d84589
k-hyeongyu <kampher@naver.com>
2025-05-26 11:44:16 +0900

    연습6 + 옵션선택 삭제 기능 추가

---
+++ src/practice/practice06/ColorBox.js
@@ -0,0 +1,15 @@
+
+function ColorBox(props) {
+    return (
+        <div className='box' style={{ backgroundColor: props.bgColor }}>
+            <button onClick={() => {
+                let temp = [...props.boxes];
+                temp.splice(props.index, 1);
+                props.setBoxes(temp);
+            }}>X</button>
+
+        </div>
+    );
+}
+
+export default ColorBox;
