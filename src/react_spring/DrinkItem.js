bd61e5cf2f65850660f7ad86e2d86f01500ef914
k-hyeongyu <kampher@naver.com>
2025-08-21 16:35:58 +0900

    [React Spring] axios 활용, 서버 REST API 통신

---
+++ src/react_spring/DrinkItem.js
@@ -0,0 +1,14 @@
+
+//function DrinkItem(props){}
+
+function DrinkItem({drinkItem}){
+
+    return(
+        <div style={{'border':'1px solid black'}}>
+            <p>이름 : {drinkItem.name}</p>
+            <p>종류 : {drinkItem.type}</p>
+        </div>
+    )
+}
+
+export default DrinkItem;
